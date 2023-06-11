7ba8ffdfd32735a884ba8f3142c4121f2f3e7b9f
Alberto Oliveira Barbosa <albertobarbosa115@gmail.com>
2023-06-11 10:35:16 -0300

    adicionado a classe Cano

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ FONTE_PLACAR = pygame.font.SysFont('arial', 50)
 
 # personagem e cenário
 IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'images','base.png')))
-IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'images','pipe.png')))
 IMAGEM_FUNDO = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'images','bg.png')))
 
 
@@ -97,7 +96,53 @@ class Passaro:
 
 
 class Cano:
-    pass
+    DISTANCIA_ENTRE_CANOS = 200
+    VELOCIDADE = 5
+    IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('assets', 'images','pipe.png')))
+
+
+    def __init__(self,x) -> None:
+        self.x = x
+        self.altura = 0
+        self.posicao_topo = 0
+        self.posicao_base = 0
+        self.CANO_TOPO = pygame.transform.flip(self.IMAGEM_CANO, False, True)
+        self.CANO_BASE = self.IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    def definir_atura(self):
+        limite_superior = 50
+        limite_inferior = 450
+        self.altura = random.randrange(limite_superior, limite_inferior)
+        # lembrando que no pygame a posisão 0 para X/Y é no topo esquerdo
+        # da tela e por isso um aumento em Y é direcionado para baixo 
+        self.posicao_topo = self.altura - self.CANO_TOPO.get_height()
+        self.posicao_base = self.altura + self.DISTANCIA_ENTRE_CANOS
+
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO(self.x, self.posicao_topo))
+        tela.blit(self.CANO_BASE(self.x, self.posicao_base))
+
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x, round(self.posicao_topo) - round(passaro.y))
+        distancia_base = (self.x - passaro.x, round(self.posicao_base) - round(passaro.y))
+
+        colisao_topo = passaro_mask.mask.overlap(topo_mask, distancia_topo)
+        colisao_base = passaro_mask.mask.overlap(base_mask, distancia_base)
+
+        if colisao_base or colisao_topo:
+            return True
+        else:
+            return False
+
 
 class Chao:
     pass
